6658389ce1c9c940ea7e670cbee6b83aa9bef06b
chenhuiru <210338370@qq.com>
2017-11-06 13:14:45 -0800

    first commit

---
+++ chenhuiru/common/js/dask.js
@@ -0,0 +1,70 @@
+
+
+/**
+ * 请求json数据
+ * @param url  json地址
+ * @param success 请求成功回调
+ * @param fail   请求失败回调
+ */
+function getJson(url, success, fail) {
+    //1.创建请求
+    var request = new XMLHttpRequest();
+    //2.配置请求
+    request.open('GET', url, true);
+    //3.发送请求
+    request.send(null);
+    //4.监听状态
+    request.onreadystatechange = function () {
+        //判断是否请求成功
+        if (request.readyState == 4 && request.status == 200) {
+            //请求成功
+            // console.log(request.responseText);
+            var obj = JSON.parse(request.responseText);
+            if (success) {
+                success(obj);
+            }
+        } else {
+            //请求失败
+            if (fail) {
+                fail(request.status);
+            }
+        }
+    }
+}
+
+
+getJson('js/product.json',function (data) {
+    for (var i=0;i<data.productSort.tablesChairs.length;i++){
+        var box=document.querySelector(".box");
+        var test = document.createElement("p");
+        test.textContent=data.productSort.tablesChairs[i];
+        box.appendChild(test);
+    }
+});
+
+getJson("js/info.json",function (datas) {
+    for (var i=0;i<datas.img.length;i++){
+        var boxTow=document.querySelector(".box-tow");
+        var div=document.createElement("div");
+        div.classList.add("list-div");
+        boxTow.appendChild(div);
+
+
+
+        var listDiv=document.querySelectorAll(".list-div")[i];
+        var span=document.createElement("span");
+        span.textContent=datas.name[i];
+        listDiv.appendChild(span);
+
+
+        var listDiv=document.querySelectorAll(".list-div")[i];
+        var span=document.createElement("b");
+        span.textContent=datas.price[i];
+        listDiv.appendChild(span);
+
+
+    }
+
+});
+
+
